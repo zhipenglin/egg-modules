74f31bf8ddb946b6b3a9b2e262328a070cbc41ef
linzhipeng <zhipeng.lin@ifchange.com>
2018-08-16 15:58:46 +0800

    v2.3.0

--- packages/egg-api-upload/app.js
+++
@@ -1,6 +0,0 @@
-const assert = require('assert');
-module.exports = app => {
-    const index = app.config.coreMiddleware.indexOf('proxy');
-    assert(index >= 0, 'proxy 中间件必须存在');
-    app.config.coreMiddleware.splice(index, 0, 'upload');
-};
